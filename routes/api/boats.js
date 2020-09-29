f8483a243d13f98c072fbca9bc3fb4d47965dd7e
Tyna William <tynawilliam@gmail.com>
2020-09-29 12:35:12 -0400

    user and boats backend routes

--- routes/api/boats.js
+++ routes/api/boats.js
@@ -2,10 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const { check } = require('express-validator');
-const { routeHandler, handleValidationErrors } = require("../utils");
+const { routeHandler, asyncHandler } = require("../utils");
 const { requireUser } = require("../utils/auth");
 
 
 const { Boat } = require('../../db/models');
 
-// router.get
+router.get('/', asyncHandler ( async(req, res) => {
+    const boats = await Boat.findAll()
+    res.json({ boats })
+}))
+
+module.exports = router;
